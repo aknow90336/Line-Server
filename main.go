73294681a71117eea0e28aba68693df220d8b780
lorence <aknow90336@gmail.com>
2023-01-14 22:20:02 +0800

    add viper

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"MessagingServer/api"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigName("app")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
 	r := gin.Default()
 	r.POST("/callback", api.CallBack)
 
